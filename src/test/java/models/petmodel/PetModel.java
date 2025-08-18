0b5dc7743b04b0aabf01d6772520903294728721
anstasiatum <anastasiya.tumakowa@gmail.com>
2025-08-18 14:45:59 +0300

    added allargsconstrustor annotation for PetModel

--- src/test/java/models/petmodel/PetModel.java
+++ src/test/java/models/petmodel/PetModel.java
@@ -1,5 +1,6 @@
 package models.petmodel;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class PetModel {
     private Long id;
     private CategoryModel category;
@@ -16,13 +18,4 @@ public class PetModel {
     private List<String> photoUrls;
     private List<TagModel> tags;
     private String status;
-
-    public PetModel(PetModel other) {
-        this.id = other.id;
-        this.category = other.category;
-        this.name = other.name;
-        this.photoUrls = other.photoUrls;
-        this.tags = other.tags;
-        this.status = other.status;
-    }
 }
